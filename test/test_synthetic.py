bde0c6b3b3457a499872c3f976f02827dbd42877
Ashwini Patil <apatil1@jhu.edu>
2015-03-18 13:28:46 -0400

    update

--- test/test_synthetic.py
+++ test/test_synthetic.py
@@ -5,10 +5,6 @@ from .. import synthetic
 
 class Test_Tools(unittest.TestCase):
     
-    def test_empty_file(self):
-        self.assertRaises(IOError, parser.parse_tool_names, StringIO(""))
-    
- 
-    
-    def test_read_size(self):
         
+    def test_read_size(self):
+
